51187a232735a0f0c0b97147d9592fe12b3f759d
Adrian  Garcia <garciadrianedu@gmail.com>
2023-04-29 13:31:16 -0500

    Initial commit

---
+++ app/src/main/java/com/cdp/adriansenaevidencia/SegundaparteActivity.java
@@ -0,0 +1,14 @@
+package com.cdp.adriansenaevidencia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SegundaparteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segundaparte);
+    }
+}
